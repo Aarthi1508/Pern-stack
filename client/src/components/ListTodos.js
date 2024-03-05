f724aa46536fea6755f52ab0bd877de69c9d0753
Aarthi1508 <Aarthi1508>
2024-03-05 12:29:24 -0600

    Edit and Delete functionalities completed

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,7 +1,25 @@
 import React, {useEffect, useState, Fragment} from "react";
+import EditTodo from "./EditTodo";
 
 const ListTodos = () => {
   const [todos, setTodos] = useState([]);
+
+  const deleteTodos = async(id) => {
+    try{
+        const deleteTodo = await fetch(`http://127.0.0.1:5000/todos/${id}`,{
+          "method":"DELETE"
+        });
+        console.log("Delete todos", deleteTodo);
+        setTodos(todos.filter(todo => todo.todo_id != id))
+    }
+    catch(err){
+      console.error(err.message);
+    }
+  }
+
+
+
+
   const getTodos = async() => {
   try{
       const response = await fetch("http://127.0.0.1:5000/todos")
@@ -13,7 +31,9 @@ const ListTodos = () => {
       console.error(err.message);
     }
   }
-  console.log("todos updated", todos)
+  // console.log("todos updated", todos)
+
+  
 
   useEffect(()=> {
     getTodos();
@@ -32,10 +52,10 @@ const ListTodos = () => {
     </thead>
     <tbody>
       {todos.map(todo => (
-        <tr>
+        <tr key={todo.todo_id}>
           <td>{todo.description}</td>
-          <td>Edit</td>
-          <td><button className="btn btn-danger">Delete</button></td>
+          <td><EditTodo todo={todo}></EditTodo></td>
+          <td><button className="btn btn-danger" onClick={() => deleteTodos(todo.todo_id)}>Delete</button></td>
         </tr>
       ))}
         
